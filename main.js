bf25880d4004481197a7736f4fc42dd245d338b8
Austin <awoods7753@gmail.com>
2022-03-11 16:38:37 -0500

    Completed Assignment

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
 // Your JavaScript code goes here!
+const theHearts = document.querySelectorAll('.like-glyph')
 
+function likeHeart(e) {
+  const heart = e.target;
+console.log(heart)
+  mimicServerCall()
+    .then(function(){
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART
+        heart.className = "activated-heart"
+      } else {
+        heart.innerText = EMPTY_HEART
+        heart.className = ""
+      }
+    })
+    .catch(function (error) {
+      const modal = document.getElementById('modal')
+      modal.className = ""
+      modal.innerText = error
+    
+      setTimeout(function() {
+          modal.className = "hidden"
+        },3000)
+      }
+)
+}
 
-
+for (const heart of theHearts) {
+  heart.addEventListener("click", likeHeart);
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
